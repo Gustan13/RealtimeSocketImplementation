2e97b0a7785d74959b5d3c683f568f176b826973
Gustavo Isoo Binder <gustan.binder@gmail.com>
2025-02-24 11:24:10 -0300

    local object system

--- stuff.js
+++ stuff.js
@@ -10,6 +10,8 @@ const ctx = canvas.getContext("2d")
 
 const input = document.getElementById("input")
 
+var localEntities = {}
+
 function send() {
     socket.emit("send message", user, input.value)
     text = input.value
@@ -17,24 +19,43 @@ function send() {
 }
 
 function localDraw() {
+    ctx.clearRect(0, 0, 200, 200)
+
     position[0] += velocity[0]
     position[1] += velocity[1]
     ctx.fillStyle = "black"
     ctx.font = "15px Consolas"
     ctx.fillRect(position[0], position[1], 20, 20)
     ctx.fillText(text, position[0], position[1] - 10)
+
+    const keys = Object.keys(localEntities)
+    keys.forEach(function (item, index) {
+        ctx.fillRect(localEntities[item][0], localEntities[item][1], 20, 20)
+        ctx.fillText(localEntities[item][2], localEntities[item][0], localEntities[item][1] - 10)
+    })
 }
 
 socket.on("draw", (entities) => {
     const keys = Object.keys(entities)
-    ctx.clearRect(0, 0, 200, 200)
+
     socket.emit("move", user, position[0], position[1])
-    localDraw()
-    keys.forEach(function (item, index) {
-        if (item != socket.id) {
-            ctx.fillRect(entities[item][0], entities[item][1], 20, 20)
-            ctx.fillText(entities[item][2], entities[item][0], entities[item][1] - 10)
-        }
+    keys.forEach((item, index) => {
+        if (item == socket.id)
+            return
+        if (!(item in localEntities))
+            localEntities[item] = [0, 0, ""]
+        localEntities[item][0] = entities[item][0]
+        localEntities[item][1] = entities[item][1]
+        localEntities[item][2] = entities[item][2]
+    })
+    console.log(localEntities)
+    // console.log(entities)
+
+    const localKeys = Object.keys(localEntities)
+    localKeys.forEach((item, index) => {
+        if (item in entities)
+            return
+        delete localEntities[item]
     })
 })
 
@@ -61,6 +82,9 @@ function down() {
 }
 
 document.onkeydown = (e) => {
+    if (user == 0)
+        return
+
     switch (e.code) {
         case "ArrowLeft":
             left()
